4caaeec82df93712212ba1cf9719ef2a0c8eec41
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-12 19:31:06 +0200

    user redux-slice added

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -4,7 +4,7 @@ const initialState = {
   username: null,
   password: null,
   name: null,
-  pictur: null,
+  picture: null,
   instagram: null,
   gmail: null,
   textarea: null,
@@ -14,6 +14,19 @@ const userSlice = createSlice({
   name: userSlice,
   initialState,
   reducers: {
-      addUser() {}
+      addUser(state, action) {
+          state.username = action.payload.username;
+          state.password = action.payload.password;
+      },
+      addUserInfo(state, action) {
+          state.name = action.payload.name,
+          state.picture = action.payload.picture;
+          state.instagram = action.payload.instagram;
+          state.gmail = action.payload.gmail;
+          state.textarea = action.payload.textarea;
+      },
+      logout(state) {
+          state = initialState;
+      },
   },
 });
